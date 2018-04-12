4a50351c0b56e68123d6785e249fec180698cb4b
Vincenzo Cimino <vicio.cimino@gmail.com>
2018-04-13 00:52:54 +0200

    Changed all

---
+++ bookmarks.js
@@ -0,0 +1,65 @@
+var bookmarks = {
+	design:{
+		name: 'design',
+		list:[
+			{
+				name: "colors",
+				priority: 5,
+				list: [
+					{
+						name: "HueSnap",
+						url: "https://www.huesnap.com/"
+					},
+					{
+						name: "Coolors",
+						url: "https://coolors.co/app"
+					},
+					{
+						name: "Adobe Color",
+						url: "https://color.adobe.com/it/create/color-wheel/"
+					},
+				]
+			},
+			{
+				name: "CSS Framework",
+				priority: 5,
+				list: [
+					{
+						name: "Spectre",
+						url: "https://www.huesnap.com/"
+					},
+					{
+						name: "Chota",
+						url: "https://coolors.co/app"
+					},
+				]
+			},
+			{
+				name: "Icons",
+				priority: 5,
+				list: [
+					{
+						name: "Feather",
+						url: "https://feathericons.com/"
+					},
+				]
+			}
+		]
+	}
+};
+
+
+/*
+{
+	name: "name of section",
+	priority: <0 to 5>,
+	list: [
+		{
+			name: "",
+			url: ""
+		},
+		{...},
+	]
+},...
+
+*/
